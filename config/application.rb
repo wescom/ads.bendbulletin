a86a818ea822656ec4e28d1a6d6cb70f04a969fb
Steve Hoffmann <shoffmann@bendbulletin.com>
2013-06-27 13:22:30 -0700

    fixing error

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module AdUpload
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
 #      g.test_framework :rspec, fixture: true
-      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+#      g.fixture_replacement :factory_girl, dir: 'spec/factories'
       g.view_specs false
       g.helper_specs false
     end
