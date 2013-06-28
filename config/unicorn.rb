7141bfb11a33566dcdceabbbd63c12f629024a28
Steve Hoffmann <shoffmann@bendbulletin.com>
2013-06-28 10:28:30 -0700

    added capistrano-unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -12,7 +12,7 @@ timeout 180
 listen "127.0.0.1:9000"
 
 # Spawn unicorn master worker for user ads (group: ads)
-user 'ads', 'ads' 
+# user 'ads', 'ads' 
 
 # Fill path to your app
 working_directory app_path
