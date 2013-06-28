c535f215a5a95928544ad2879ff9f66d4386c3ab
Steve Hoffmann <shoffmann@bendbulletin.com>
2013-06-28 16:58:35 -0700

    trying to get unicorn to work

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,29 @@
+# Needed to have capistrano run bundle install on deploy
+require 'bundler/capistrano'
+
 set :application, "ads.bendbulletin.com"
-set :repository,  "git@github.com:wescom/ads.bendbulletin.git"
+
+# SSH configuration
 default_run_options[:pty] = true
+
+# Source repository taken for deployments
+set :repository,  "git@github.com:wescom/ads.bendbulletin.git"
+set :ssh_options, { :forward_agent => true }
+set :bundle_flags, ''
+
+# Destination of deployments
+# set :deploy_to, '/home/myuser/myapp_deployed'
+# set :deploy_via, :copy
 
 role :web, "172.16.128.125"                       # Your HTTP server, Apache/etc
 role :app, "172.16.128.125"                       # This may be the same as your `Web` server
 role :db,  "172.16.128.125", :primary => true     # This is where Rails migrations will run
 
-set :ssh_options, { :forward_agent => true }
+# if you want to clean up old releases on each deploy uncomment this:
+after 'deploy:restart', 'deploy:cleanup'
 
-# Needed to have coaistrano run bundle install on deploy
-require 'bundler/capistrano'
+# Unicorn tasks
 require 'capistrano-unicorn'
-
 after 'deploy:restart', 'unicorn:reload'    # app IS NOT preloaded
 after 'deploy:restart', 'unicorn:restart'   # app preloaded
 
