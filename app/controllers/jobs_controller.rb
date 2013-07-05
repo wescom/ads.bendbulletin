ae4e5912e5e15a46cc3615ae3820819a90eb55c6
Steve Hoffmann <shoffmann@bendbulletin.com>
2013-07-05 09:54:37 -0700

    adjusted menu security

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :require_admin, :only => [:edit, :update, :destroy]
+  before_filter :require_admin, :only => [:index, :show, :edit, :update, :destroy]
   before_filter :require_edit, :only => [:index, :show]
 
   def index
