3adf3d545da7c34d6db377ad8d6829a31735f55c
Steve Hoffmann <shoffmann@bendbulletin.com>
2015-05-12 15:23:33 -0700

    alter script to kill unicorn process before starting new

--- lib/tasks/start_unicorn.sh
+++ lib/tasks/start_unicorn.sh
@@ -2,6 +2,7 @@
 
 echo "$(date +%m/%d/%y\ %T)"
 cd /u/apps/ads.bendbulletin.com/current
-bundle install
+bundle install>>/tmp/null
 
+rm /u/apps/ads.bendbulletin.com/current/tmp/pids/unicorn.pid 
 bundle exec unicorn_rails -c config/unicorn/production.rb -E production -D
