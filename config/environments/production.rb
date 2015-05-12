71e713f074308d487a020ad90e72c4fb4d317afb
Steve Hoffmann <shoffmann@bendbulletin.com>
2015-05-12 14:54:33 -0700

    changed back to using GMail for sending email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ AdUpload::Application.configure do
    config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp-relay.gmail.com",
     port: 587,
     domain: "gmail.com",
     authentication: "plain",
