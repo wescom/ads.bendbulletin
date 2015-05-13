a047ee7a6fb2879239dd6bbad8facb9adec6adc1
Steve Hoffmann <shoffmann@bendbulletin.com>
2015-05-12 21:16:22 -0700

    change from unicorn_rails to unicorn in startup script

--- lib/tasks/start_unicorn.sh
+++ lib/tasks/start_unicorn.sh
@@ -4,5 +4,5 @@ echo "$(date +%m/%d/%y\ %T)"
 cd /u/apps/ads.bendbulletin.com/current
 bundle install>>/tmp/null
 
-bundle exec unicorn_rails -c config/unicorn/production.rb -E production -D
+bundle exec unicorn -c config/unicorn/production.rb -E production -D
 chmod 777 /u/apps/ads.bendbulletin.com/shared/pids/unicorn.pid
