8b4063d85c6d68cdfd12117e0711f76f186cd930
Steve Hoffmann <shoffmann@bendbulletin.com>
2020-09-20 22:28:49 -0700

    switch wescompapers to bendbulletin domain for email auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ AdUpload::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "wescompapers.com",
+    domain: "bendbulletin.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
